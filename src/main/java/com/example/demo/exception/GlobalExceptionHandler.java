fb5bbe919f796e94eea3487f35b1bf77b2d2436c
madhu-idev <madhubala.selvam@gmail.com>
2026-04-23 17:21:26 +0000

    Added exception handling and validation

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -13,6 +13,6 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGeneral(Exception ex) {
-        return ResponseEntity.internalServerError().body("Something went wrong");
+        return ResponseEntity.internalServerError().body("Something went wrong!");
     }
 }
